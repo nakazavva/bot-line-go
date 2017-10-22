5bc504bcc2874f81b44e225f077f1e532d837572
Toshihide Nakazawa <nakazavva@gmail.com>
2017-10-22 20:37:18 +0900

    Repeate received message with "バジ〜"

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
@@ -18,5 +20,34 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "こんにちはバジ〜!!")
+	proxyURL, _ := url.Parse(os.Getenv("FIXIE_URL"))
+	client := &http.Client{
+		Transport: &http.Transport{Proxy: http.ProxyURL(proxyURL)},
+	}
+
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+		linebot.WithHTTPClient(client),
+	)
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				m := message.Text + "バジ〜"
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(m)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
 }
